6cc7197fc9c4655af1cf7e93e4c17c041d0a260e
Marc <marcjacobroy@gmail.com>
2020-11-02 12:59:20 -0500

    StudySetServiceImplTest

---
+++ src/test/java/FlashCard/service/CourseServiceImplTest.java
@@ -0,0 +1,81 @@
+package FlashCard.service;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import FlashCard.pojos.Course;
+import FlashCard.pojos.Instructor;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CourseServiceImplTest {
+	
+	private CourseServiceImpl courseService;
+	
+	@Mock
+	private CustomCacheServiceSimpleInMemory<Course> customCache; 
+	
+	List<Course> courseList;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		courseList = new ArrayList<Course>();
+		
+		Instructor instructor1 = new Instructor("marc");
+		Instructor instructor2 = new Instructor("mike");
+		
+		Course course1 = new Course("german", instructor1);
+		Course course2 = new Course("french", instructor1);
+		Course course3 = new Course("russian", instructor2);
+		Course course4 = new Course("portuguese", instructor2);
+		
+		courseList.add(course1);
+		courseList.add(course2);
+		courseList.add(course3);
+		courseList.add(course4);
+		
+		when((customCache.retrieveAllItems())).thenReturn(courseList);
+		courseService = new CourseServiceImpl(customCache);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		courseList.clear();
+	}
+
+	@Test
+	public void createcourseTest() {
+		Instructor instructor = new Instructor("hi");
+		Course testCourse = courseService.createCourse("arabic", instructor);
+		assertEquals("Should create course object", "arabic", testCourse.getCourseName());
+		assertEquals("Should create course object", instructor, testCourse.getInstructor());
+		verify(customCache).addToCache(testCourse);
+	}
+	
+	@Test
+	public void getcourseListTest() {
+		assertEquals("Should return full list of courses", courseList, courseService.getAllCourses());
+	}
+	
+
+}
---
+++ src/test/java/FlashCard/service/StudySetServiceImplTest.java
@@ -0,0 +1,76 @@
+package FlashCard.service;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import FlashCard.pojos.StudySet;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StudySetServiceImplTest {
+	
+	private StudySetServiceImpl studySetService;
+	
+	@Mock
+	private CustomCacheServiceSimpleInMemory<StudySet> customCache; 
+	
+	List<StudySet> studySetList;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		studySetList = new ArrayList<StudySet>();
+		
+		StudySet studySet1 = new StudySet();
+		StudySet studySet2 = new StudySet();
+		StudySet studySet3 = new StudySet();
+		StudySet studySet4 = new StudySet();
+		
+		studySetList.add(studySet1);
+		studySetList.add(studySet2);
+		studySetList.add(studySet3);
+		studySetList.add(studySet4);
+		
+		when((customCache.retrieveAllItems())).thenReturn(studySetList);
+		studySetService = new StudySetServiceImpl(customCache);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		studySetList.clear();
+	}
+
+	@Test
+	public void createstudySetTest() {
+		StudySet teststudySet = studySetService.createStudySet();
+		assertEquals("Should create studySet object", 4, teststudySet.getStudySetId());
+		verify(customCache).addToCache(teststudySet);
+	}
+	
+	@Test
+	public void getstudySetListTest() {
+		assertEquals("Should return full list of studySets", studySetList, studySetService.getAllStudySets());
+	}
+	
+
+}
+
